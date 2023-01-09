23083c831f5cb45d22326b16704df4d4d35eaddc
Damaso Sanoja <damasosanoja@gmail.com>
2023-01-09 17:04:10 -0400

    First commit

---
+++ connection.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID       int64
+	Title    string
+	Artist   string
+	Price    float32
+	Quantity int64
+}
+
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
